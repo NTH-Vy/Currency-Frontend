f0cf1c234f25a38d24d8b0a3a1445a04fc067d82
NTH-Vy <nguyenvy01052005@gmail.com>
2026-06-30 22:35:29 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,25 @@
 import type { NextConfig } from "next";
 
 const laravelOrigin =
-  process.env.NEXT_PUBLIC_API_BASE?.replace(/\/$/, "") ?? "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_API_BASE?.replace(/\/$/, "") || "http://127.0.0.1:8000";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "https",
+        hostname: "currency-backend-hv03.onrender.com",
+        pathname: "/storage/**",
       },
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/storage/**",
       },
     ],
   },
+
   async rewrites() {
     return [
       {
